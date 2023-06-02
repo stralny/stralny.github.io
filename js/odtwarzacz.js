6127994b4ac5ca25000e91b7162ef17bc2580d09
stralny <jakubkraus97@gmail.com>
2023-06-02 23:52:41 +0700

    upd

--- js/odtwarzacz.js
+++ js/odtwarzacz.js
@@ -1,25 +1,26 @@
-var audio = {
-    init: function () {
-      var $that = this;
-      $(function () {
-        $that.components.media();
-      });
-    },
-    components: {
-      media: function (target) {
-        var media = $("audio.fc-media", target !== undefined ? target : "body");
-        if (media.length) {
-          media.mediaelementplayer({
-            audioHeight: 40,
-            features: ["playpause", "current", "duration", "progress", "tracks"],
-            alwaysShowControls: true,
-            timeAndDurationSeparator: "<span></span>",
-            iPadUseNativeControls: true,
-            iPhoneUseNativeControls: true,
-            AndroidUseNativeControls: true
-          });
-        }
-      }
-    }
+var audio = {    
+      init: function() {        
+      var $that = this;        
+          $(function() {            
+              $that.components.media();        
+          });    
+      },
+      components: {        
+          media: function(target) {            
+              var media = $('audio.fc-media', (target !== undefined) ? target : 'body');            
+              if (media.length) {                
+                  media.mediaelementplayer({                    
+                      audioHeight: 40,
+                      features : ['playpause', 'current', 'duration', 'progress', 'tracks', 'fullscreen'],
+                      alwaysShowControls      : true,
+                      timeAndDurationSeparator: '<span></span>',
+                      iPadUseNativeControls: true,
+                      iPhoneUseNativeControls: true,
+                      AndroidUseNativeControls: true                
+                  });            
+              }        
+          },
+              
+      },
   };
-  audio.init();  
+  audio.init();
